0636c0ae7ddc6cba599c080ec1706e526ce162bf
Loganw94 <loganwogoman@gmail.com>
2018-06-03 01:22:39 -0400

    fix

--- src/Game/game.py
+++ src/Game/game.py
@@ -1,7 +1,7 @@
 
 class Game:
     def __init__(self, PY):
-        self.init_objs = []
+        self.game_objs = []
         self.key_file = "Game/Files/default_keys.json"
         self.game_name = "Temp Game"
         self.game_size = (640, 480)
